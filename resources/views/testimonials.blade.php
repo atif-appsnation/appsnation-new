2ec56be586f8b175e4d50fda43f1d8ee49d2b914
atif-hanif <94978139+atif-hanif@users.noreply.github.com>
2024-12-18 09:20:13 +0500

    Testimonials updated

--- resources/views/testimonials.blade.php
+++ resources/views/testimonials.blade.php
@@ -77,6 +77,58 @@
     </section>
     <!-- End Testimonials -->
 
+    {{-- edit by talha --}}
+    <!-- Start TrustPilot Testimonials -->
+    <section class="reviews-block py-5">
+        <div class="container">
+            <div class="row">
+				<div class="col-12">
+                    <div class="common-heading w-tdxt text-center">
+                        <div data-text="Awards" class="dark-bg-text bg-text text-center">
+                            <h2 class="mb30">TrustPilot reviews</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="pilot-slider owl-carousel owl-theme">
+                        @foreach ($trustPilotTestimonial as $testimonial)
+                            <div class="testimonial-items">
+                                <div class="item">
+                                    <div class="card h-100 d-flex">
+                                        <div class="card-body">
+                                            <h6 class="text-muted text-center">{{ $testimonial->name }}</h6>
+                                            {{-- <svg viewBox="0 0 16 16" fill="currentColor" class="icon_icon__ECGRl" xmlns="http://www.w3.org/2000/svg" width="14px" height="14px"><path fill-rule="evenodd" clip-rule="evenodd" d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16Zm-.888-4.44 5.603-5.87-.723-.69-4.897 5.13-2.388-2.388L4 8.449l3.112 3.112Z"></path></svg>
+											<span>Verified</span> --}}
+                                            <img src="images/svg/stars-5.svg" alt="" style="height:20px;">
+
+                                            {{-- <img src="uploads/awards/0ba32fb4e7536c7803da3bfecaa3f681.webp" class="award-img"> --}}
+                                            <h5 class="text-center pt-3">{{ $testimonial->title }}</h5>
+                                            <p class="text-center pt-2">{{ $testimonial->comments }}</p>
+                                            <div class="row">
+                                                <div class="d-inline-block w-75">
+                                                    <p class="text-start  pt-2">{{ str_replace("Date of experience:", "",$testimonial->dateOfExperience) }}</p>
+                                                </div>
+                                                <div class=" d-inline-block w-25">
+                                                    <p class="text-end  pt-2">{{ $testimonial->location }}</p>
+                                                </div>
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </section>
+    <!-- End TrustPilot Testimonials -->
+    {{-- end edit by talha --}}
+
     <!-- Start Technologies -->
     <section class="technologies certificates py-5">
         <div class="container">
@@ -202,56 +254,6 @@
     </section>
     <!--End Badges-->
 
-    {{-- edit by talha --}}
-    <!-- Start TrustPilot Testimonials -->
-    <section class="reviews-block py-5">
-        <div class="container">
-            <div class="row">
-				<div class="col-12">
-                    <div class="common-heading w-tdxt text-center">
-                        <div data-text="Awards" class="dark-bg-text bg-text text-center">
-                            <h2 class="mb30">TrustPilot reviews</h2>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12">
-                    <div class="pilot-slider owl-carousel owl-theme">
-                        @foreach ($trustPilotTestimonial as $testimonial)
-                            <div class="award-items">
-                                <div class="item">
-                                    <div class="card h-100 d-flex">
-                                        <div class="card-body">
-                                            <h6 class="text-muted text-center">{{ $testimonial->name }}</h6>
-                                            {{-- <svg viewBox="0 0 16 16" fill="currentColor" class="icon_icon__ECGRl" xmlns="http://www.w3.org/2000/svg" width="14px" height="14px"><path fill-rule="evenodd" clip-rule="evenodd" d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16Zm-.888-4.44 5.603-5.87-.723-.69-4.897 5.13-2.388-2.388L4 8.449l3.112 3.112Z"></path></svg>
-											<span>Verified</span> --}}
-                                            <img src="images/svg/stars-5.svg" alt="" style="height:20px;">
-
-                                            {{-- <img src="uploads/awards/0ba32fb4e7536c7803da3bfecaa3f681.webp" class="award-img"> --}}
-                                            <h5 class="text-center pt-3">{{ $testimonial->title }}</h5>
-                                            <p class="text-center pt-2">{{ $testimonial->comments }}</p>
-
-                                            <div class=" d-inline-block w-75">
-                                                <p class="text-start  pt-2">{{ str_replace("Date of experience:", "",$testimonial->dateOfExperience) }}</p>
-                                            </div>
-											<div class=" d-inline-block w-25">
-                                                <p class="text-end  pt-2">{{ $testimonial->location }}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        @endforeach
-
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-    </section>
-    <!-- End TrustPilot Testimonials -->
-
-    {{-- end edit by talha --}}
     <!--Start Let's Work Together-->
     <div class="row py-5 justify-content-center hire-developer">
         <div class="col-lg-7">
